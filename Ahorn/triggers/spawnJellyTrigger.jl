03093919fbb8ce3b97132120295d0a5fa22f2bc0
norah486 <32691116+norah486@users.noreply.github.com>
2020-10-23 20:38:29 -0300

    Added Spawn Jellyfish Trigger

---
+++ Ahorn/triggers/spawnJellyTrigger.jl
@@ -0,0 +1,14 @@
+﻿module YetAnotherHelperSpawnJellyTrigger
+
+using ..Ahorn, Maple
+
+@mapdef Trigger "YetAnotherHelper/SpawnJellyTrigger" SpawnJellyTrigger(x::Integer, y::Integer, width::Integer=Maple.defaultTriggerWidth, height::Integer=Maple.defaultTriggerHeight, onlyOnce::Bool=true)
+
+const placements = Ahorn.PlacementDict(
+    "Spawn Jelly (Yet Another Helper)" => Ahorn.EntityPlacement(
+        SpawnJellyTrigger,
+        "rectangle"
+    )
+)
+
+end
---
+++ YetAnotherHelper/Triggers/SpawnJellyTrigger.cs
@@ -0,0 +1,41 @@
+﻿using Celeste.Mod.Entities;
+using Microsoft.Xna.Framework;
+
+namespace Celeste.Mod.YetAnotherHelper.Triggers
+{
+    [CustomEntity("YetAnotherHelper/SpawnJellyTrigger")]
+    class SpawnJellyTrigger : Trigger
+    {
+        public bool OnlyOnce { get; private set; }
+
+        public bool Activated { get; private set; }
+
+        public SpawnJellyTrigger(EntityData data, Vector2 offset) : this(data, offset, data.Bool("onlyOnce", true), data.Bool("activated", false))
+        {
+        }
+
+        public SpawnJellyTrigger(EntityData data, Vector2 offset, bool onlyOnce, bool activated) : base(data, offset)
+        {
+            OnlyOnce = onlyOnce;
+            Activated = activated;
+        }
+
+        public override void OnEnter(Player player)
+        {
+            base.OnEnter(player);
+
+            Level level = player.SceneAs<Level>();
+            if(!OnlyOnce)
+                level.Add(new Glider(player.Position, false, false));
+
+            else
+            {
+                if(!Activated)
+                {
+                    level.Add(new Glider(player.Position, false, false));
+                    Activated = true;
+                }
+            }
+        }
+    }
+}
