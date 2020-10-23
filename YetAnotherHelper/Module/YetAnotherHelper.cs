09ca15a6ddd40c690e8e576faed6838d4cbd41a1
norah486 <32691116+norah486@users.noreply.github.com>
2020-10-23 12:51:24 -0300

    Initial commit.

---
+++ YetAnotherHelper/Module/YetAnotherHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Celeste.Mod.YetAnotherHelper.Module
+{
+    public class YetAnotherHelperModule : EverestModule
+    {
+
+        public static YetAnotherHelperModule Instance;
+
+        public YetAnotherHelperModule()
+        {
+            Instance = this;
+        }
+
+        public override Type SettingsType => null;
+
+        public override Type SaveDataType => null;
+
+        public override Type SessionType => null;
+
+        public override void Load()
+        {
+        }
+
+        public override void Initialize()
+        {
+        }
+
+        public override void LoadContent(bool firstLoad)
+        {
+        }
+
+        public override void Unload()
+        {
+        }
+
+    }
+}
