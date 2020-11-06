70b84ad5337fca70167f50be7674981be615a50e
norah486 <32691116+norah486@users.noreply.github.com>
2020-11-05 22:25:30 -0300

    Make Sticky Jellyfish stick to walls

--- YetAnotherHelper/Entities/StickyJellyfish.cs
+++ YetAnotherHelper/Entities/StickyJellyfish.cs
@@ -1,12 +1,12 @@
 ﻿using Celeste.Mod.Entities;
 using Microsoft.Xna.Framework;
+using System;
 
 namespace Celeste.Mod.YetAnotherHelper.Entities
 {
     [CustomEntity("YetAnotherHelper/StickyJellyfish")]
     public class StickyJellyfish : Glider
     {
-
         public StickyJellyfish(EntityData data, Vector2 offset) : base(data, offset)
         {
         }
@@ -14,11 +14,13 @@ namespace Celeste.Mod.YetAnotherHelper.Entities
         public static void Load()
         {
             On.Celeste.Glider.Update += StickyJellyUpdateHook;
+            On.Celeste.Glider.OnCollideH += StickyJellyHorizontalHook;
         }
 
         public static void Unload()
         {
             On.Celeste.Glider.Update -= StickyJellyUpdateHook;
+            On.Celeste.Glider.OnCollideH -= StickyJellyHorizontalHook;
         }
 
         public static void StickyJellyUpdateHook(On.Celeste.Glider.orig_Update orig, Glider self)
@@ -28,6 +30,17 @@ namespace Celeste.Mod.YetAnotherHelper.Entities
                 self.Speed.Y = -1.67f;
             }
             orig(self);
+        }
+
+        private static void StickyJellyHorizontalHook(On.Celeste.Glider.orig_OnCollideH orig, Glider self, CollisionData data)
+        {
+            if (self is StickyJellyfish)
+            {
+                if (data.Hit is DashSwitch)
+                    (data.Hit as DashSwitch).OnDashCollide(null, Vector2.UnitX * (float)Math.Sign(self.Speed.X));
+                self.Speed.X = 0f;
+            }
+            orig(self, data);
         }
     }
 }
