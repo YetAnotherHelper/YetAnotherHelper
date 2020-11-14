7b25b5caa75819505aa10afba4c3321cfb7743ab
WoofWoofDoggo <barkbarkpupper@gmail.com>
2020-11-13 21:56:25 -0500

    Update README and code style change
    Add description to README and move curly brackets to new lines.

--- YetAnotherHelper/Entities/SpikeJumpThruController.cs
+++ YetAnotherHelper/Entities/SpikeJumpThruController.cs
@@ -7,9 +7,11 @@ using System.Linq;
 // Based on RainbowSpinnerColorController
 // from SpringCollab2020 / MaxHelpingHand
 
-namespace Celeste.Mod.YetAnotherHelper.Entities {
+namespace Celeste.Mod.YetAnotherHelper.Entities 
+{
     [CustomEntity("YetAnotherHelper/SpikeJumpThruController")]
-    class SpikeJumpThruController : Entity {
+    class SpikeJumpThruController : Entity 
+    {
         private static bool SpikeHooked;
         private static SpikeJumpThruController CurrentController;
         private static SpikeJumpThruController NextController;
@@ -18,7 +20,8 @@ namespace Celeste.Mod.YetAnotherHelper.Entities {
         public SpikeJumpThruController(EntityData data, Vector2 offset) : this(data.Bool("persistent", false), offset) 
             { }
 
-        public SpikeJumpThruController(bool persistent, Vector2 offset) : base(offset) {
+        public SpikeJumpThruController(bool persistent, Vector2 offset) : base(offset) 
+        {
             YetAnotherHelperModule.Instance.Session.SpikeJumpThruHooked = persistent;
 
             Add(new TransitionListener
@@ -31,33 +34,40 @@ namespace Celeste.Mod.YetAnotherHelper.Entities {
 
         }
 
-        public static void Load() {
+        public static void Load() 
+        {
             On.Celeste.Level.LoadLevel += OnLoadLevelHook;
         }
 
-        public static void Unload() {
+        public static void Unload() 
+        {
             On.Celeste.Level.LoadLevel -= OnLoadLevelHook;
         }
 
-        public override void Update() {
+        public override void Update() 
+        {
             base.Update();
-            if(TransitionProgress == -1f && CurrentController == null) {
+            if(TransitionProgress == -1f && CurrentController == null) 
+            {
                 CurrentController = this;
                 NextController = null;
             } 
         }
 
-        public override void Awake(Scene scene) {
+        public override void Awake(Scene scene) 
+        {
             base.Awake(scene);
 
             NextController = this;
-            if(!SpikeHooked) {
+            if(!SpikeHooked) 
+            {
                 On.Celeste.Spikes.OnCollide += OnCollideHook;
                 SpikeHooked = true;
             }
         }
 
-        public override void Removed(Scene scene) {
+        public override void Removed(Scene scene) 
+        {
             base.Removed(scene);
 
             CurrentController = NextController;
@@ -65,34 +75,41 @@ namespace Celeste.Mod.YetAnotherHelper.Entities {
 
             TransitionProgress = -1f;
 
-            if(SpikeHooked && CurrentController == null) {
+            if(SpikeHooked && CurrentController == null) 
+            {
                 On.Celeste.Spikes.OnCollide -= OnCollideHook;
                 SpikeHooked = false;
             }
         }
 
-        public override void SceneEnd(Scene scene) {
+        public override void SceneEnd(Scene scene) 
+        {
             base.SceneEnd(scene);
 
             CurrentController = NextController = null;
-            if(SpikeHooked) {
+            if(SpikeHooked) 
+            {
                 On.Celeste.Spikes.OnCollide -= OnCollideHook;
                 SpikeHooked = false;
             }
         }
 
-        private static void OnLoadLevelHook(On.Celeste.Level.orig_LoadLevel orig, Level level, Player.IntroTypes introType, bool isFromLoader) {
+        private static void OnLoadLevelHook(On.Celeste.Level.orig_LoadLevel orig, Level level, Player.IntroTypes introType, bool isFromLoader) 
+        {
             orig(level, introType, isFromLoader);
 
-            if(YetAnotherHelperModule.Instance.Session.SpikeJumpThruHooked && !level.Session.LevelData.Entities.Any(entity => entity.Name == "YetAnotherHelper/SpikeJumpThruController")) {
+            if(YetAnotherHelperModule.Instance.Session.SpikeJumpThruHooked && !level.Session.LevelData.Entities.Any(entity => entity.Name == "YetAnotherHelper/SpikeJumpThruController")) 
+            {
                 level.Add(new SpikeJumpThruController(YetAnotherHelperModule.Instance.Session.SpikeJumpThruHooked, Vector2.Zero));
                 level.Entities.UpdateLists();
             }
         }
 
-        private static void OnCollideHook(On.Celeste.Spikes.orig_OnCollide orig, Spikes spikes, Player player) {
+        private static void OnCollideHook(On.Celeste.Spikes.orig_OnCollide orig, Spikes spikes, Player player) 
+        {
             // If the player is picking up a holdable, don't kill them.
-            if(player.StateMachine.State == 8) {
+            if(player.StateMachine.State == 8) 
+            {
                 return;
             }
 
